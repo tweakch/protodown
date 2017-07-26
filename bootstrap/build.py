34d5f7ba1e85e4747777075fec06041318d2a64c
tweakch <alex.klee@outlook.com>
2017-07-27 01:14:47 +0200

    add recursive parseCommand method

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -3,3 +3,21 @@ from string import Template
 from bootstrap.commands import CommandsConfig, ProtodownCommand
 
 """recursively add subparsers"""
+def parseCommands(parser, commandsConfig):
+    subparsers = parser.add_subparsers(title="my subparsers") #add subparsers to parser
+
+    for command, config in commandsConfig.items():
+        command_parser=subparsers.add_parser(
+            command,
+            help=config['help'],
+            aliases=config['aliases'])
+
+        if 'subcommands' in config:
+            subcommands=config['subcommands']
+            parseCommands(command_parser, subcommands)
+        if 'arguments' in config:
+            arguments=config['arguments']
+
+
+    return subparsers
+

ab554642a7d101db15e36f832c4ce9c6d1d6a2df
tweakch <alex.klee@outlook.com>
2017-07-27 01:18:13 +0200

    hookup command line arguments to functions

--- bootstrap/app.py
+++ bootstrap/app.py
@@ -26,3 +26,26 @@ def generate(arguments):
 def run(arguments):
     return "Starting application..."
 
+class Protodown(object):
+    """generate prototypes from markdown"""
+    def __init__(self):
+        super(Protodown, self).__init__()
+        self.arguments=Arguments()
+
+        #This should be in the kernel.
+        self.arguments['config'].set_defaults(func=config);
+        self.arguments['resolve'].set_defaults(func=resolve);
+        self.arguments['generate'].set_defaults(func=generate);
+        self.arguments['run'].set_defaults(func=run);
+        self.arguments['lookup'].set_defaults(func=lookup);
+
+        self.args=self.arguments.get_args()
+
+    def print_help(self):
+        self.arguments.print_help()
+#group = parser.add_argument_group('Available protodown commands')
+#group.add_argument('init', help="Create an empty Protodown repository or reinitialize an existing one")
+#group.add_argument('config', help="Configure your protodown repository")
+#group.add_argument('resolve', help="Find solutions for your prototype")
+#group.add_argument('generate', help="Let the magic happen")
+#group.add_argument('run', help="Start your prototype")

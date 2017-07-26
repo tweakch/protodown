0ae092d0cb3f9c0905a5467d721f71fe007e5802
tweakch <alex.klee@outlook.com>
2017-07-27 01:15:15 +0200

    introduce Arguments class

--- bootstrap/build.py
+++ bootstrap/build.py
@@ -21,3 +21,25 @@ def parseCommands(parser, commandsConfig):
 
     return subparsers
 
+class Arguments():
+    """docstring for Arguments."""
+
+    def __init__(self):
+        self.parser=argparse.ArgumentParser(
+            description="Parser description",
+            epilog="Epilog of the parser class",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        self.commandsConfig=CommandsConfig()
+        self.subparsers=parseCommands(self.parser, self.commandsConfig)
+
+    def __getitem__(self,key):
+        if key in self.subparsers.choices:
+            return self.subparsers.choices[key]
+        else:
+             raise KeyError('Command "{0}" was not configured.'.format(key))
+
+    def get_args(self):
+        return self.parser.parse_args()
+
+    def print_help(self):
+        self.parser.print_help()
